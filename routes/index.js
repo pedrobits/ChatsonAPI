8514038007608146f0cd13220c3b35a1ef14ff7d
pedrobits <pedroksk2@gmail.com>
2024-03-05 16:59:49 -0300

    feat: :sparkles: Add new routes, login and create a new User

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-
+import userController from '../controllers/user.controller.js'
 
 const routes = (app) => {
 	app.get('/', (req, res) => {
 		res.send('ChatsonAPI 1.0');
 	  });
+
+	  app.post('/createNewUser', userController.createNewUser)
+	  app.post('/login', userController.loginUser)
 }
 
 export default routes;
